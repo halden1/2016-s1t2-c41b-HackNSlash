54891b9ba05df0bbb9dc89c9cff72cd86c69d652
J-Ceguerra <james.ceguerra@gmail.com>
2016-05-17 13:06:47 +1000

    Started on the Input Manager for movement
    Added Initial Global Manager Class
    Added Initial Input Manager Class

---
+++ Assets/Scripts/GlobalManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalManager : MonoBehaviour {
+
+    private static GlobalManager _globalManager;
+    private static InputManager _inputManager;
+
+    void Awake() {
+        if (_globalManager != null && _globalManager != this) {
+            Destroy(this.gameObject);
+        }
+        else {
+            _globalManager = this;
+        }
+    }
+
+    void Start () {
+        _inputManager = GameObject.FindObjectOfType<InputManager>();
+    }
+	
+	public static InputManager Input() {
+        return _inputManager;
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+    private static InputManager _inputManager;
+
+    public static InputManager Instance {
+        get {
+            return _inputManager;
+        }
+    }
+    
+    void Awake() {
+        if (_inputManager != null && _inputManager != this) {
+            Destroy(this.gameObject);
+        } else {
+            _inputManager = this;
+        }
+    }
+    
+    void Update() {
+
+    }
+}
