86fc5c8b2234df0aa4fb994d0390d6652d70f44a
halden1 <halden.junk@gmail.com>
2016-06-07 10:23:19 +1000

    fixing level up Script
    the level up script was poorly optimized

--- Assets/Scripts/LevelUp.cs
+++ Assets/Scripts/LevelUp.cs
@@ -14,6 +14,7 @@ public class LevelUp : MonoBehaviour {
 
 
     #region Private Variables
+    private PlayerStats playerStats;
 
     private int currentLevel = 0;
     private int currentExp = 0;
@@ -22,23 +23,22 @@ public class LevelUp : MonoBehaviour {
     #endregion
     void Start () {
         //get exp and level from player stats 
-        currentLevel = GameObject.FindGameObjectWithTag(playerStatsTag).GetComponent<PlayerStats>().getLevel();
-        currentExp = GameObject.FindGameObjectWithTag(playerStatsTag).GetComponent<PlayerStats>().getExp();
+        playerStats = FindObjectOfType<PlayerStats>();
+        currentLevel = playerStats.getLevel();
+        currentExp = playerStats.getExp();
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (currentExp >= expToLevelUp)
         {
-            currentLevel++;
             expToLevelUp += expToLevelUp;//exp to level up doubles each level
-
-            GameObject.FindGameObjectWithTag(playerStatsTag).GetComponent<PlayerStats>().addLevel(1);//save these to the player stat class
-            GameObject.FindGameObjectWithTag(playerStatsTag).GetComponent<PlayerStats>().addHealth(healthPerLevel);
-            GameObject.FindGameObjectWithTag(playerStatsTag).GetComponent<PlayerStats>().addAttack(attackPerLevel);
-            GameObject.FindGameObjectWithTag(playerStatsTag).GetComponent<PlayerStats>().addDefence(defencePerLevel);
-            GameObject.FindGameObjectWithTag(playerStatsTag).GetComponent<PlayerStats>().addSpeed(speedPerLevel);
-            GameObject.FindGameObjectWithTag(playerStatsTag).GetComponent<PlayerStats>().addExp(currentExp - GameObject.FindGameObjectWithTag(playerStatsTag).GetComponent<PlayerStats>().getExp());
+            playerStats.addLevel(1);//save these to the player stat class
+            playerStats.addHealth(healthPerLevel);
+            playerStats.addAttack(attackPerLevel);
+            playerStats.addDefence(defencePerLevel);
+            playerStats.addSpeed(speedPerLevel);
+            playerStats.addExp(currentExp - playerStats.getExp());
         }
 	}
     public void forceLevelup() { currentExp = expToLevelUp; }
